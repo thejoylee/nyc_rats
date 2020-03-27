3da75707e9f6af6ac1d9c2c2dfa6f0e401674bab
thejoylee <62118591+thejoylee@users.noreply.github.com>
2020-03-26 17:59:49 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,37 +1,38 @@
-#map.js
 'use strict'
+
 console.log('Loaded map.js')
-mapboxgl.accessToken='pk.eyJ1IjoidGhlam95bGVlIiwiYSI6ImNrN3BmZnZ5NDBqYjQzbW1pNGs0MWFiejkifQ.9stnbzBJqom6XYjX73VYcA'
+
+mapboxgl.accessToken = 'YOUR TOKEN HERE'
 
 let map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/thejoylee/ck89c4hme003w1ims5f5ktulp',
-	center: [-73.96216,40.80779],
-	zoom:16
+    container: 'map',
+    style: 'YOUR STYLE URL HERE',
+    center: [-73.96216,40.80779],
+    zoom: 16
 })
 
 let navigation = new mapboxgl.NavigationControl({
-	showCompass: false
+    showCompass: false
 })
 map.addControl(navigation, 'top-left')
 
-let scale = new mapboxgl.ScaleControl ({
-	maxWidth: 80,
-	unit: 'imperial'
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
 })
-map.addControl(scale,'bottom-right')
+map.addControl(scale, 'bottom-right')
 
-let geolocate = new mapbox.gl.GeolocateControl({
-	positionOptions: {
-		enableHighAccuracy: true
-	}, 
-	trackUserLocation: true,
-	showUserLocation: true,
-	fitBoundsOptions: {
-	}
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
 })
-map.addControl(geolocate,'top-left')
+map.addControl(geolocate, 'top-left')
 
-geolocate.on('gelocate',function(event) {	
+geolocate.on('geolocate', function(event) {
+
 })
-
