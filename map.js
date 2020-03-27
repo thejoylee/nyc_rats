42ebebd964718db925073640c685f1ad5800be00
thejoylee <62118591+thejoylee@users.noreply.github.com>
2020-03-26 18:11:22 -0700

    Update map.js

--- map.js
+++ map.js
@@ -36,3 +36,45 @@ map.addControl(geolocate, 'top-left')
 geolocate.on('geolocate', function(event) {
 
 })
+
+map.on('click', function(event) {
+    // rat sightings from layer data
+    let features = map.queryRenderedFeatures({layers:['rat-sightings']})
+    console.log(features)
+    
+    // location of the click
+    let current_location = [event.lngLat.lng, event.lngLat.lat]
+    console.log(current_location)
+    
+    // if there aren't any features don't continue
+    if (features.length == 0) return
+    
+    // create variables to hold the closest feature found so far
+    let closest_distance = Infinity
+    let closest_feature = null
+    
+    //we're going to check each feature
+    for (let feature of features) {
+        // calculate the distance using turf
+        let distance = turf.distance(turf.point(feature.geometry.coordinates), turf.point(current_location))
+        
+        // if distance is less than closest distance we've seen so far, update the variables
+        if (distance < closest_distance) {
+            closest_distance = distance
+            closest_feature = feature
+        }
+    }
+    
+    // closest_distance should now be set to the minimum value
+    // closest_feature should be set to the feature itself
+    console.log("Closest feature:", closest_feature.geometry.coordinates, "(", closest_distance, "m)")
+    
+    // additional handler code goes here
+    
+    // calculate bearing
+    let bearing = turf.bearing(turf.point(current_location), turf.point(closest_feature.geometrry.coordinates)
+    console.log("Bearing:", bearing)
+    
+})
+    
+})
