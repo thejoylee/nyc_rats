6f8cedbdb5cb50ad18cc1765ca3f908d2d62c006
thejoylee <62118591+thejoylee@users.noreply.github.com>
2020-03-26 17:49:51 -0700

    Add files via upload

---
+++ map.js
@@ -0,0 +1,37 @@
+map.js
+'use strict'
+console.log('Loaded map.js')
+mapboxgl.accessToken='pk.eyJ1IjoidGhlam95bGVlIiwiYSI6ImNrN3BmZnZ5NDBqYjQzbW1pNGs0MWFiejkifQ.9stnbzBJqom6XYjX73VYcA'
+
+let map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/thejoylee/ck89c4hme003w1ims5f5ktulp',
+	center: [-73.96216,40.80779],
+	zoom:16
+})
+
+let navigation = new mapboxgl.NavigationControl({
+	showCompass: false
+})
+map.addControl(navigation, 'top-left')
+
+let scale = new mapboxgl.ScaleControl ({
+	maxWidth: 80,
+	unit: 'imperial'
+})
+map.addControl(scale,'bottom-right')
+
+let geolocate = new mapbox.gl.GeolocateControl({
+	positionOptions: {
+		enableHighAccuracy: true
+	}, 
+	trackUserLocation: true,
+	showUserLocation: true,
+	fitBoundsOptions: {
+	}
+})
+map.addControl(geolocate,'top-left')
+
+geolocate.on('gelocate',function(event) {	
+})
+
